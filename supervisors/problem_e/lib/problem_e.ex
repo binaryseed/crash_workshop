47b064e143c2f3d6bb4c77137dd3f6f2bfe67fc6
Vince Foley <vincefoley@gmail.com>
2017-03-04 20:52:14 -0800

    Solution supE

--- supervisors/problem_e/lib/problem_e.ex
+++ supervisors/problem_e/lib/problem_e.ex
@@ -6,15 +6,22 @@ defmodule ProblemE do
   @doc """
   Start a Supervisor for Agents.
   """
-  def start_link(opts \\ [])
+  def start_link(opts \\ [restart: :temporary]) do
+    children = [Supervisor.Spec.worker(Agent, [], opts)]
+    Supervisor.start_link(children, [strategy: :simple_one_for_one] ++ opts)
+  end
 
   @doc """
   Start an Agent with a fun.
   """
-  def start_child(sup, fun, opts \\ [])
+  def start_child(sup, fun, opts \\ []) do
+    Supervisor.start_child(sup, [fun])
+  end
 
   @doc """
   Start an Agent with module, function and arguments.
   """
-  def start_child(sup, mod, fun, args, opts \\ [])
+  def start_child(sup, mod, fun, args, opts \\ []) do
+    Supervisor.start_child(sup, [mod, fun, args])
+  end
 end
