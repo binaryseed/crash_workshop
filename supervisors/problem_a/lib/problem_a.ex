9e72e50d08c827d872bedf9a3d5f044dae624b4b
Vince Foley <vincefoley@gmail.com>
2017-03-04 19:20:39 -0800

    Solution for A

--- supervisors/problem_a/lib/problem_a.ex
+++ supervisors/problem_a/lib/problem_a.ex
@@ -7,6 +7,7 @@ defmodule ProblemA do
   Start a task that is run again if it crashes
   """
   def start_link(fun) do
-    Task.start_link(fun)
+    children = [Supervisor.Spec.worker(Task, [fun], restart: :transient)]
+    Supervisor.start_link(children, strategy: :one_for_one, max_restarts: 1)
   end
 end
