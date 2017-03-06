2681c25b1476699385a935f443369671136b31f4
Vince Foley <vincefoley@gmail.com>
2017-03-05 17:30:38 -0800

    Solution supK

--- supervisors/problem_k/lib/problem_k.ex
+++ supervisors/problem_k/lib/problem_k.ex
@@ -7,5 +7,27 @@ defmodule ProblemK do
   Write a Task supervising process that starts a single task with
   `Task.start_link(fun)` and restarts it if it exits.
   """
-  def start_link(fun)
+
+  def start_link(fun) do
+    Task.start_link(fn ->
+      Process.flag(:trap_exit, true)
+      init(fun)
+    end)
+  end
+
+  def init(fun) do
+    {:ok, task} = Task.start_link(fun)
+    loop(task, fun)
+  end
+
+  def loop(task, fun) do
+    receive do
+      {:EXIT, ^task, :shutdown} ->
+        exit(:shutdown)
+      {:EXIT, ^task, :crash} ->
+        init(fun)
+      {:EXIT, _from, :crash} ->
+        loop(task, fun)
+    end
+  end
 end
