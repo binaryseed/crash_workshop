b9353089af045af74b7d725183771c9f03b43c09
Vince Foley <vincefoley@gmail.com>
2017-03-07 22:17:13 -0800

    solution agentC

--- agents/problem_c/lib/problem_c.ex
+++ agents/problem_c/lib/problem_c.ex
@@ -13,12 +13,25 @@ defmodule ProblemC do
   @doc """
   Call task.
   """
-  def call(task, request, timeout)
+  def call(task, request, timeout) do
+    send(task, {__MODULE__, self(), request})
+    receive do
+      {__MODULE__, response} ->
+        response
+      {:EXIT, from, reason} ->
+        exit {reason, {__MODULE__, :call, [task, request, timeout]}}
+    after
+      timeout ->
+        exit {:timeout, {__MODULE__, :call, [task, request, timeout]}}
+    end
+  end
 
   @doc """
   Reply to call
   """
-  def reply(from, response)
+  def reply(from, response) do
+    send(from, {__MODULE__, response})
+  end
 
   @doc false
   def loop() do
