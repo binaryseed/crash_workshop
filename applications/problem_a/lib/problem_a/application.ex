746259153c5d38052c8146427faa568fb469ace2
Vince Foley <vincefoley@gmail.com>
2017-03-07 21:36:16 -0800

    solution appA

--- applications/problem_a/lib/problem_a/application.ex
+++ applications/problem_a/lib/problem_a/application.ex
@@ -1,13 +1,23 @@
 defmodule ProblemA.Application do
-  @moduledoc false
-
-  alias ProblemA.{Alice, Bob, Data}
-
   use Application
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-    children = [worker(Data, []), worker(Alice, []), worker(Bob, [])]
+    children = [
+      supervisor(ProblemA.Sup, []),
+    ]
     Supervisor.start_link(children, [strategy: :one_for_one])
+  end
+end
+
+defmodule ProblemA.Sup do
+  def start_link() do
+    import Supervisor.Spec, warn: false
+    children = [
+      worker(ProblemA.Data, []),
+      worker(ProblemA.Alice, []),
+      worker(ProblemA.Bob, []),
+    ]
+    Supervisor.start_link(children, [strategy: :one_for_one, max_restarts: 3])
   end
 end
